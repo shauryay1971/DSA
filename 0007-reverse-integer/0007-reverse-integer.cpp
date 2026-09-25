1017126ac618df0ebbe4e0930f2e2b660bf90c01
shauryay1971 <shauryay7017@gmail.com>
2026-09-26 00:26:55 +0530

    Time: 0 ms (100%), Space: 8.3 MB (97.95%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
     int reverse(int x) {
-        //INT_MAX is defined using #define INT_MAX 2147483647
-        bool sign=true;
-        unsigned int p=x;
-        if(x<0){
-            sign=false;
-           p=-p;//edge case if x is INT_MIN then -x can nnot be represented in int
-        }
+        // INT_MAX is defined using #define INT_MAX 2147483647
 
-       int num=0;
-       int k=0;
-        while(p>0){
-            num=num*10+(p%10);
-            p=p/10;
-            k++;
-            if(k==9 && num>214748364 && p>0){
+        int num = 0;
+
+        while (x != 0) { // shld run for negative values
+
+            if (abs(num) > 214748364 && x != 0) { // if x is zero then the number has ended
                 return 0;
-            }else if(k==9 && num==214748364 &&p>0){
-                if(p>8 && sign==true){
-                    return 0;
+            } else if (abs(num) == 214748364 && (x > 8 || x < -9)) {
 
-                }else if(sign==false && p>9){
-                    return 0;
-
-                }
-
+                return 0;
             }
-
-        }
-        if(sign){
-            return num;
-
-        }else{
-            return -num;
+            num = num * 10 + (x % 10);
+            x = x / 10;
         }
 
-        
+        return num;
     }
 };
